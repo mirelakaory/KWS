6a07e9e57df0ffdd3fc7c234ea566023e5367f06
Mirela Hangai <mirelakaory@gmail.com>
2026-02-11 01:21:06 +0900

    up

--- menu.js
+++ menu.js
@@ -3,10 +3,6 @@ function toggleMenu() {
       .querySelector('.nav_list')
       .classList.toggle('active');
   }
-  document.querySelectorAll('.nav_link').forEach(link => {
-    link.addEventListener('click', () => {
-      document.querySelector('.nav_list').classList.remove('active');
-    });
-  });
+
   
   
